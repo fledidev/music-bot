6116d5f5268ff10e17fec5c3179c2c7f27c7b01f
LetsCodeManh <manh.ng@icloud.com>
2023-12-06 20:45:07 +0100

    feat: flask setup

--- main.py
+++ main.py
@@ -1,19 +1,19 @@
-from flask import Flask
-from dotenv import load_dotenv
-import bot
+from flask import Flask, render_template
+# from dotenv import load_dotenv
+# import bot
 import os
 
 app = Flask(__name__)
+app.template_folder = 'web/templates'
+app.static_folder = 'web/static'
 
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def home():
+    return render_template("index.html")
 
 
 if __name__ == '__main__':
-    load_dotenv()
-    bot.discord_bot.run_bot(os.getenv('DISCORD_API_KEY'))
+    # oad_dotenv()
+    # bot.discord_bot.run_bot(os.getenv('DISCORD_API_KEY'))
     app.run(debug=True)
-
-
